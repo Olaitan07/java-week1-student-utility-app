ba3c0e98db13034df44f06af5786b931ccdec6bf
Olaitan07 <olabanjotboy@gmail.com>
2026-02-07 22:27:55 +0100

    initial project setup

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+
+
+
+    public static void main(String[] args){
+
+        System.out.println("==== Student utility app ====");
+        System.out.println("1. Add two numbers");
+        System.out.println("2. Check if a number is even or odd");
+        System.out.println("3. Print numbers from 1 to N");
+        System.out.println("4. Display days of the week");
+        System.out.println("5. Exit");
+
+        System.out.print("Enter your choice: ");
+
+
+
+
+    }
+}
