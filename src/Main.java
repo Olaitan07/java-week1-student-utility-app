68ffd2968af44f43172db235265cf7e635870312
Olaitan07 <olabanjotboy@gmail.com>
2026-02-07 22:42:49 +0100

    adding scanner to print the menu

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,10 @@ public class Main {
 
 
     public static void main(String[] args){
+        // Scanner object to read value from the system
+        Scanner sc = new Scanner(System.in);
 
+        //print the menu
         System.out.println("==== Student utility app ====");
         System.out.println("1. Add two numbers");
         System.out.println("2. Check if a number is even or odd");
@@ -14,6 +17,28 @@ public class Main {
         System.out.println("5. Exit");
 
         System.out.print("Enter your choice: ");
+        // read the user input which is an integer
+        int choice = sc.nextInt();
+        // use if else statement to perform the action based on the user choice
+        if(choice == 1){
+            System.out.println("add two numbers");
+        }
+            else if(choice == 2){
+                System.out.println("check if a number is even or odd");
+            }
+            else if(choice == 3){
+                System.out.println("print numbers from 1 to N");
+            }
+            else if(choice == 4){
+                System.out.println("display days of the week");
+            }
+            else if(choice == 5){
+                System.out.println("exit");
+            }
+            else {
+                System.out.println("invalid choice");
+            }
+
 
 
 
